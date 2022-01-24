41d18b1e348f03b88e9723943dbefd93565fd459
Doyeon04 <rlaehdus0411@naver.com>
2022-01-24 14:40:07 +0900

    글쓰기 post, 글 가져오기 get

--- src/component/content/Content.js
+++ src/component/content/Content.js
@@ -2,20 +2,53 @@ import React from "react";
 import styles from "./Content.module.css";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Content = (props) => {
-  axios
-    .get("http://localhost:8080/api/posts")
-    .then((response) => {
-      // response
-      console.log(response);
-    })
-    .catch((error) => {
-      // 오류발생시 실행
-    })
-    .then(() => {
-      // 항상 실행
-    });
+
+  /*
+  for (let i = 132; i < 135; i++) {
+
+     let i = 0;
+
+     
+    axios
+      .delete(`http://localhost:8080/api/posts/${i}`)
+      .then((response) => {
+        // handle success
+        console.log(response);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      })
+      .then(() => {
+        // always executed
+      });
+  }
+
+  */
+
+
+  useEffect(() => {
+   
+   
+
+    axios
+      .get("http://localhost:8080/api/posts")
+      .then((response) => {
+        // response
+        console.log("content:", response);
+      })
+      .catch((error) => {
+        // 오류발생시 실행
+      })
+      .then(() => {
+        // 항상 실행
+      });
+  }, []);
+
+  //localhost:8080/api/posts/2
 
   return (
     <div className={styles.container}>
