8fd0ca8bff777a726c2b99d68c17cc21025eec87
Doyeon04 <rlaehdus0411@naver.com>
2022-01-24 13:49:56 +0900

    get 받아오기

--- src/component/content/Content.js
+++ src/component/content/Content.js
@@ -1,8 +1,22 @@
 import React from "react";
 import styles from "./Content.module.css";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 const Content = (props) => {
+  axios
+    .get("http://localhost:8080/api/posts")
+    .then((response) => {
+      // response
+      console.log(response);
+    })
+    .catch((error) => {
+      // 오류발생시 실행
+    })
+    .then(() => {
+      // 항상 실행
+    });
+
   return (
     <div className={styles.container}>
       <div className={styles.writeBtnBox}>
