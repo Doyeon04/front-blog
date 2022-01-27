9e2cb6400bf1f696add3b60d7ff83aaa8a887461
joinin20190104 <cym2009@naver.com>
2022-01-27 18:42:31 +0900

    삭제 버튼 누르면 메인화면으로 이동

--- src/component/PostDetail.js
+++ src/component/PostDetail.js
@@ -1,12 +1,33 @@
 import { useParams, useLocation } from "react-router";
+
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect  } from "react";
 import Header from "./header/Header";
 import styles from "./PostDetail.module.css";
+import {Link,useNavigate} from "react-router-dom";
 
 function PostDetail(props) {
   const { postId } = useParams();
   const { state } = useLocation();
+  const navigate = useNavigate();
+  const onDelete = (event) => {
+    event.preventDefault();
+    axios
+    .delete(`http://localhost:8080/api/posts/${postId}`)
+    .then((response) => {
+      // response
+      console.log("content:", response);
+    })
+    .catch((error) => {
+      // 오류발생시 실행
+    })
+    .then(() => {
+      // 항상 실행
+      navigate("/");
+    });
+    //원래 페이지로 돌아가기 
+
+  }
 
   console.log(props.location);
 
@@ -31,8 +52,11 @@ function PostDetail(props) {
         <Header></Header>
       </div>
       <div className={styles.BtnContainer}>
-        <button>수정</button>
-        <button>삭제</button>
+      <button>수정</button>
+     <div>
+       <button onClick = {onDelete}>삭제</button>
+       </div>
+           
       </div>
     </div>
   );
