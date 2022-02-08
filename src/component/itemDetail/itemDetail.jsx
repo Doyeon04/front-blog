e42e345e978b044be53a5398864bd9f1d93cdd1e
Doyeon04 <rlaehdus0411@naver.com>
2022-02-08 21:51:55 +0900

    itemDetail 대문자로 변경

--- src/component/itemDetail/itemDetail.jsx
+++ src/component/itemDetail/itemDetail.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import styles from './itemDetail.module.css';
+import React from "react";
+import styles from "./itemDetail.module.css";
 
-
-const itemDetail = ({item}) => {
-  console.log(item.content.split(" ")[0])
+const ItemDetail = ({ item }) => {
+  console.log(item.content.split(" ")[0]);
   const str = item.content.split(" ");
-  const itemContent = str.slice(1).join(' ').toString();
- 
+  const itemContent = str.slice(1).join(" ").toString();
+
   return (
-    <div className = {styles.itemDetail_container}>
-      <div className = {styles.item_container}>
+    <div className={styles.itemDetail_container}>
+      <div className={styles.item_container}>
         <h3>{item.title}</h3>
         <p>{itemContent}</p>
       </div>
-      <div className = {styles.comments_box}>
-        <span className = {styles.comments_number}>0</span>
+      <div className={styles.comments_box}>
+        <span className={styles.comments_number}>0</span>
         <span>comments</span>
       </div>
     </div>
   );
 };
 
-export default itemDetail;
+export default ItemDetail;
